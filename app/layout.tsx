abf7e1d03f88bf514e438983e2fe5321fec40848
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2023-10-11 11:49:21 +0800

    refactor: 💡 update `Header` component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,11 @@
 'use client'
 
-import { Fragment } from 'react'
-
-import { Popover, Transition } from '@headlessui/react'
-import { ChevronDownIcon } from '@heroicons/react/20/solid'
 import clsx from 'clsx'
 import 'focus-visible'
 import { Inter } from 'next/font/google'
-import Link from 'next/link'
 
-import { Main, Paragraph } from '@components'
-import { ILayoutProps, IRootLayoutPageLinksData } from '@interfaces'
+import { Header, Main } from '@components'
+import { ILayoutProps } from '@interfaces'
 import { AuthProvider } from '@providers'
 
 import '@styles/globals.css'
@@ -21,64 +16,11 @@ const inter = Inter({
 })
 
 const RootLayout = ({ children }: ILayoutProps): JSX.Element => {
-  const pageLinks: IRootLayoutPageLinksData[] = [
-    { label: 'Home', href: '/', description: 'Go to Home' },
-    { label: 'Notes', href: '/notes', description: 'Go to Notes' },
-  ]
-
   return (
     <AuthProvider>
       <html lang="en">
-        <body className={clsx(inter.className, 'px-3 py-6')}>
-          <header className="flex justify-center">
-            <Popover className="relative">
-              {({ open }) => (
-                <>
-                  <Popover.Button className="inline-flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900">
-                    <span>Menu</span>
-                    <ChevronDownIcon className="h-5 w-5" aria-hidden="true" />
-                  </Popover.Button>
-
-                  <Transition
-                    as={Fragment}
-                    show={open}
-                    enter="transition ease-out duration-200"
-                    enterFrom="opacity-0 translate-y-1"
-                    enterTo="opacity-100 translate-y-0"
-                    leave="transition ease-in duration-150"
-                    leaveFrom="opacity-100 translate-y-0"
-                    leaveTo="opacity-0 translate-y-1"
-                  >
-                    <Popover.Panel
-                      className="absolute left-1/2 z-10 mt-5 flex w-screen max-w-max -translate-x-1/2 px-4"
-                      static
-                    >
-                      <div className="w-screen max-w-sm flex-auto rounded-3xl bg-white p-4 text-sm leading-6 shadow-lg ring-1 ring-gray-900/5">
-                        {pageLinks.map(({ label, href, description }) => (
-                          <div
-                            key={href}
-                            className="relative rounded-lg p-4 hover:bg-gray-50"
-                          >
-                            <Link
-                              href={href}
-                              className="font-semibold text-gray-900"
-                            >
-                              {label}
-                              <span className="absolute inset-0" />
-                            </Link>
-                            <Paragraph
-                              className="mt-1 text-gray-600"
-                              message={description}
-                            />
-                          </div>
-                        ))}
-                      </div>
-                    </Popover.Panel>
-                  </Transition>
-                </>
-              )}
-            </Popover>
-          </header>
+        <body className={clsx(inter.className, 'md:px-3 md:py-6')}>
+          <Header />
           <Main>{children}</Main>
         </body>
       </html>
---
+++ components/layouts/Header.tsx
@@ -0,0 +1,132 @@
+import { useState } from 'react'
+
+import { Dialog } from '@headlessui/react'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+import Image from 'next/image'
+import Link from 'next/link'
+
+import { PAGE_LINKS } from '@config'
+
+const Header = (): JSX.Element => {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState<boolean>(false)
+
+  const navigation = PAGE_LINKS
+
+  return (
+    <header className="bg-white md:sticky md:top-0 relative z-10">
+      <nav
+        className="mx-auto flex max-w-7xl items-center justify-between gap-x-6 p-6 lg:px-8"
+        aria-label="Global"
+      >
+        <div className="flex lg:flex-1">
+          <a href="#" className="-m-1.5 p-1.5">
+            <span className="sr-only">Your Company</span>
+            <Image
+              className="h-8 w-auto"
+              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+              width={38}
+              height={32}
+              alt=""
+            />
+          </a>
+        </div>
+        <div className="hidden lg:flex lg:gap-x-12">
+          {navigation.map(item => (
+            <Link
+              key={item.name}
+              href={item.href}
+              className="text-sm font-semibold leading-6 text-gray-900"
+            >
+              {item.name}
+            </Link>
+          ))}
+        </div>
+        <div className="flex flex-1 items-center justify-end gap-x-6">
+          <Link
+            href="#"
+            className="hidden lg:block lg:text-sm lg:font-semibold lg:leading-6 lg:text-gray-900"
+          >
+            Log in
+          </Link>
+          <Link
+            href="#"
+            className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Sign up
+          </Link>
+        </div>
+        <div className="flex lg:hidden">
+          <button
+            type="button"
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+            onClick={() => setMobileMenuOpen(true)}
+          >
+            <span className="sr-only">Open main menu</span>
+            <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+          </button>
+        </div>
+      </nav>
+      <Dialog
+        as="div"
+        className="lg:hidden"
+        open={mobileMenuOpen}
+        onClose={setMobileMenuOpen}
+      >
+        <div className="fixed inset-0 z-10" />
+        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+          <div className="flex items-center gap-x-6">
+            <a href="#" className="-m-1.5 p-1.5">
+              <span className="sr-only">Your Company</span>
+              <Image
+                className="h-8 w-auto"
+                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                width={38}
+                height={32}
+                alt=""
+              />
+            </a>
+            <a
+              href="#"
+              className="ml-auto rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Sign up
+            </a>
+            <button
+              type="button"
+              className="-m-2.5 rounded-md p-2.5 text-gray-700"
+              onClick={() => setMobileMenuOpen(false)}
+            >
+              <span className="sr-only">Close menu</span>
+              <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+            </button>
+          </div>
+          <div className="mt-6 flow-root">
+            <div className="-my-6 divide-y divide-gray-500/10">
+              <div className="space-y-2 py-6">
+                {navigation.map(item => (
+                  <a
+                    key={item.name}
+                    href={item.href}
+                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  >
+                    {item.name}
+                  </a>
+                ))}
+              </div>
+              <div className="py-6">
+                <a
+                  href="#"
+                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                >
+                  Log in
+                </a>
+              </div>
+            </div>
+          </div>
+        </Dialog.Panel>
+      </Dialog>
+    </header>
+  )
+}
+
+export default Header
