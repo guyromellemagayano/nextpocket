dbe6d11e184bed9748ce37a4ec3927dfd5e50f66
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2023-10-11 11:50:19 +0800

    refactor: 💡 update data config with interfaces

--- config/index.ts
+++ config/index.ts
@@ -1,4 +1,4 @@
 export { NOTES_PAGE_API_URL, NOTE_PAGE_API_URL } from './api'
 export { AXIOS_INSTANCE } from './axios'
-export { NOTES_COLLECTION_FORM_DATA } from './data'
+export { NOTES_COLLECTION_FORM_DATA, PAGE_LINKS } from './data'
 export { API_HOST_LOCAL } from './env'
