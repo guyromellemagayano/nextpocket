58dafb83c1783015c41d9c2a53115a245e955b01
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2023-10-11 11:55:32 +0800

    fix: 🐛 update `images.domains` in next config

--- next.config.js
+++ next.config.js
@@ -3,7 +3,12 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['images.unsplash.com', 'avatars.githubusercontent.com'],
+    dangerouslyAllowSVG: true,
+    domains: [
+      'images.unsplash.com',
+      'avatars.githubusercontent.com',
+      'tailwindui.com',
+    ],
   },
 }
 
