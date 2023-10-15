21448b0cb492027253e52b722525f505200218e2
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2023-10-15 10:27:26 +0800

    feat: 🎸 add auth features and functions

--- providers/AuthProvider.tsx
+++ providers/AuthProvider.tsx
@@ -1,7 +1,12 @@
+'use client'
+
 import { SessionProvider } from 'next-auth/react'
 
 import { ILayoutProps } from '@interfaces'
 
+/**
+ * Provides authentication context to the app.
+ */
 const AuthProvider = ({ children }: ILayoutProps): JSX.Element => (
   <SessionProvider>{children}</SessionProvider>
 )
