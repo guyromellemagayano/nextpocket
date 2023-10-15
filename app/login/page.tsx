aedb6a41a4b46c99133e80f1dc4ac0a07ae0293d
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2023-10-15 10:25:10 +0800

    fix: 🐛 update login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,37 +1,64 @@
 'use client'
 
-import { GithubSvgImage, Heading, Paragraph } from '@components'
 import { FC } from 'react'
+
+import {
+  Button,
+  GithubSvgImage,
+  Heading,
+  Message,
+  Paragraph,
+} from '@components'
+import { useRedirect } from '@hooks'
+import clsx from 'clsx'
+import { signIn } from 'next-auth/react'
 
 /**
  * Render the Login page
  */
 const LoginPage: FC = (): JSX.Element => {
-  return (
-    <div className="flex min-h-full flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8">
-      <div className="sm:mx-auto sm:w-full sm:max-w-md">
-        <Heading
-          className="mt-12 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900"
-          size="h2"
-        >
-          Sign in to your account
-        </Heading>
-      </div>
+  const { session, status } = useRedirect()
 
-      <div className="mt-6 sm:mx-auto sm:w-full sm:max-w-sm">
-        <div className="bg-white px-6 py-12 sm:px-12">
-          <div className="grid grid-cols-1 gap-4 gap">
-            <a
-              href="#"
-              className="flex w-full items-center justify-center gap-3 rounded-md bg-[#24292F] px-3 py-1.5 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#24292F]"
+  return (
+    <div className="flex flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8 min-h-screen h-full">
+      <div className="flex min-h-full flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8">
+        <div
+          className={clsx(
+            'sm:mx-auto sm:w-full sm:max-w-md',
+            status !== 'unauthenticated' && 'flex items-center flex-col',
+          )}
+        >
+          {status === 'loading' ? (
+            <Message className="my-3 text-center text-lg font-medium leading-9 tracking-tight text-gray-600">
+              Loading...
+            </Message>
+          ) : (
+            <Heading
+              className="text-center text-2xl font-bold leading-9 tracking-tight text-gray-900"
+              size="h2"
             >
-              <GithubSvgImage className="h-5 w-5" />
-              <Paragraph className="text-sm font-semibold leading-6">
-                Github
-              </Paragraph>
-            </a>
-          </div>
+              Sign in to your account
+            </Heading>
+          )}
         </div>
+
+        {!session && status === 'unauthenticated' && (
+          <div className="sm:mx-auto sm:w-full sm:max-w-sm py-12 sm:px-6 lg:px-8">
+            <div className="bg-white px-6 py-6 sm:px-12">
+              <div className="grid grid-cols-1 gap-4 gap">
+                <Button
+                  className="flex w-full items-center justify-center gap-3 rounded-md bg-[#24292F] px-3 py-1.5 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#24292F]"
+                  onClick={() => signIn('github', { callbackUrl: '/notes' })}
+                >
+                  <GithubSvgImage className="h-5 w-5" />
+                  <Paragraph className="text-sm font-semibold leading-6">
+                    Github
+                  </Paragraph>
+                </Button>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   )
