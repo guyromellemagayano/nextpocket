ebc0b71bef0b853d8aa9f7f6da46b890e1a3b5e0
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2023-10-15 10:36:32 +0800

    feat: 🎸 add arrayFilter util function

--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,4 @@
+export { default as arrayFilter } from './arrayFilter'
 export { default as datetime } from './datetime'
 export { default as fetcher } from './fetcher'
 export { http } from './http'
