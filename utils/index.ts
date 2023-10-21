55bd58fecb2d909de2ed30163edd58f8bfaf8439
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2023-10-21 14:07:40 +0800

    fix: 🐛 remove redundant `http` util function

--- utils/index.ts
+++ utils/index.ts
@@ -1,4 +1,3 @@
 export { default as arrayFilter } from './arrayFilter'
 export { default as datetime } from './datetime'
 export { default as fetcher } from './fetcher'
-export { http } from './http'
