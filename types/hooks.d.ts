59da563b9069b156bd09bcd1ecd26ae191ca8340
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2023-10-22 04:18:27 +0800

    refactor: 💡 update helper function type locations, update deps

--- types/hooks.d.ts
+++
@@ -1,9 +0,0 @@
-import { Session } from 'next-auth'
-
-import { TPageLinks } from '@types'
-
-export type TUseRedirect = {
-  navigation: TPageLinks[]
-  session: Session
-  status: string
-}
--- types/lib.d.ts
+++
@@ -1,3 +0,0 @@
-export type TAxiosErrorResponse = {
-  message?: string
-}
