33f57908581c925c67aad416b35fb230fc0e45f3
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2023-10-22 03:08:16 +0800

    refactor: 💡 update hook function type locations

--- hooks/useRedirect.ts
+++ hooks/useRedirect.ts
@@ -1,8 +1,19 @@
+import { Session } from 'next-auth'
 import { useSession } from 'next-auth/react'
 
-import { PAGE_LINKS } from '@config'
-import { TUseRedirect } from '@types'
+import { PAGE_LINKS, TPageLinks } from '@config/data'
 
+type TUseRedirect = {
+  navigation: TPageLinks[]
+  session: Session
+  status: string
+}
+
+/**
+ * A custom hook that returns navigation links, session data, and status.
+ *
+ * @returns An object containing navigation links, session data, and status.
+ */
 const useRedirect = (): TUseRedirect => {
   const { data: session, status } = useSession()
 
