192a6529a26b7d11d9b6d01f8f8171486ff5b022
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2023-10-22 20:31:15 +0800

    fix: 🐛 update `PocketBase` migrations

---
+++ pb_migrations/1697928699_updated_users.js
@@ -0,0 +1,33 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("sem9zti83ul9nou")
+
+  // remove
+  collection.schema.removeField("bydnsmdu")
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("sem9zti83ul9nou")
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "bydnsmdu",
+    "name": "notes",
+    "type": "relation",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "collectionId": "y9429efb59e35d4",
+      "cascadeDelete": false,
+      "minSelect": null,
+      "maxSelect": null,
+      "displayFields": null
+    }
+  }))
+
+  return dao.saveCollection(collection)
+})
