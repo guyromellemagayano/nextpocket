2fd7193400cf60fc85703f2b8fd086d03c47df59
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2023-10-22 20:00:33 +0800

    fix: 🐛 update util function imports and tests

--- utils/fetcher.ts
+++ utils/fetcher.ts
@@ -1,6 +1,6 @@
-import request from '@helpers/request'
+import { request } from '@helpers'
 
-type TFetcherProps = <T>(url: string) => Promise<void | Awaited<T>>
+type TFetcherProps = <T>(url: string) => Promise<void | Awaited<T> | any>
 
 /**
  * Fetches data from the specified URL using a GET request.
@@ -11,7 +11,7 @@ type TFetcherProps = <T>(url: string) => Promise<void | Awaited<T>>
 const fetcher: TFetcherProps = url =>
   request({
     method: 'GET',
-    url,
+    url
   })
 
 export default fetcher
