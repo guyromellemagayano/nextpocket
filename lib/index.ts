dc6de0416aef23622f56af0f961048fb7f2ffc3b
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2023-10-23 08:51:03 +0800

    test: 💍 add `axios` tests

--- lib/index.ts
+++ lib/index.ts
@@ -1,4 +1,7 @@
-export { default as axiosInstance, handleError } from './axios'
+export { AXIOS_INSTANCE, default as axiosInstance, handleError } from './axios'
 export { default as nextAuthOptions } from './nextauth'
 
-export type { AXIOS_INSTANCE, TAxiosErrorResponse } from './axios'
+export type {
+  TAxiosErrorResponse,
+  AXIOS_INSTANCE as TAxiosInstance
+} from './axios'
